920c69cddc9a590c23166d886569c3a5a6822c80
Tanawin <UMD-DEV01-device@outlook.com>
2020-04-13 00:32:59 +0700

    java rejex

--- src/Test/matcher.java
+++ src/Test/matcher.java
@@ -1,4 +1,17 @@
 package Test;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class matcher {
+    public static void main(String args[]){
+
+        Pattern test1 = Pattern.compile("((a)(b(c)))");
+        Matcher m = test1.matcher("I adoc love dog godabcabc ");
+        while (m.find()){
+            System.out.println(m.group());
+        }
+        System.out.println("finish");
+
+    }
 }
